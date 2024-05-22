219127bc6d7ed0ed5e33baa6111014d1bb7d2482
kegan <kegan.cherry2@gmail.com>
2024-05-21 20:54:11 -0500

    initial commit

---
+++ src/main/java/com/cherrytree/password_manager_server/PasswordManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.cherrytree.password_manager_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordManagerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cherrytree/password_manager_server/PasswordManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherrytree.password_manager_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
